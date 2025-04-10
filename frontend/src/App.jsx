447928f04e4f4f6003b8db0fbc4b8642191c836f
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2025-04-10 14:23:32 +0530

    frontend complete inside in using props , axios,DateTimePicker, useState

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
-import  React, { useEffect, useState } from 'react'
-import './App.css'
-import axios from "axios"
-import DateTimePicker from 'react-datetime-picker';
-
+import React, { useEffect, useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import axios from "axios";
+import DateTimePicker from "react-datetime-picker";
 
 function App() {
+  const [reminderMsg, setReminderMsg] = useState("");
+  const [remindAt, setRemindAt] = useState();
 
-  const [reminderMsg, setReminderMsg] = useState("")
-  const [ remindAt, setRemindAt] = useState()
-
-  const addReminder = () =>{
-
-  }
+  const addReminder = () => {};
 
   return (
     <div className="App">
       <div className="homepage">
-
         <div className="homepage_header">
           <h1>Remind Me 🙋‍♂️</h1>
-          <input type="text" placeholder="Reminder notes here..." value={reminderMsg} onChange={e => setReminderMsg(e.target.value)} />
-          <DateTimePicker 
+          <input
+            type="text"
+            placeholder="Reminder notes here..."
+            value={reminderMsg}
+            onChange={(e) => setReminderMsg(e.target.value)}
+          />
+          <DateTimePicker
             value={remindAt}
             onChange={setRemindAt}
             minDate={new Date()}
@@ -30,11 +32,45 @@ function App() {
             monthPlaceholder="MM"
             yearPlaceholder="YYYY"
           />
-          <div className="button" onClick={addReminder}>Add Reminder</div>
+          <div className="button" onClick={addReminder}>
+            Add Reminder
+          </div>
         </div>
+
+        <div className="homepage_body">
+          <div className="reminder_card">
+            <h2>Reminder Note</h2>
+            <h3>Remind Me at:</h3>
+            <p>10/04/2025 @ 2PM</p>
+            <div className="button">Delete</div>
+          </div>
+
+          <div className="reminder_card">
+            <h2>Reminder Note</h2>
+            <h3>Remind Me at:</h3>
+            <p>10/04/2025 @ 2PM</p>
+            <div className="button">Delete</div>
+          </div>
+
+
+
+          <div className="reminder_card">
+            <h2>Reminder Note</h2>
+            <h3>Remind Me at:</h3>
+            <p>10/04/2025 @ 2PM</p>
+            <div className="button">Delete</div>
+          </div>
+
+          <div className="reminder_card">
+            <h2>Reminder Note</h2>
+            <h3>Remind Me at:</h3>
+            <p>10/04/2025 @ 2PM</p>
+            <div className="button">Delete</div>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
